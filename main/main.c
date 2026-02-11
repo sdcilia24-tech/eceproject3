775497264797382e9d3a99ca8c55227364ca27f1
sebCil <sdcilia24@gmail.com>
2026-02-11 16:31:31 -0500

    "almost working intermittent mode"

--- main/main.c
+++ main/main.c
@@ -8,6 +8,7 @@
 #include <hd44780.h>
 #include <esp_idf_lib_helpers.h>
 #include <inttypes.h>
+#include "driver/ledc.h"
 
 #define ignitionLED 11
 #define engineLED 3
@@ -20,6 +21,14 @@
 #define adcAtten ADC_ATTEN_DB_12
 #define bitWidth ADC_BITWIDTH_12
 #define wiperPoten ADC_CHANNEL_3
+#define LEDC_TIMER LEDC_TIMER_0
+#define LEDC_MODE LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO (7)
+#define LEDC_CHANNEL LEDC_CHANNEL_0
+#define LEDC_DUTY_RES LEDC_TIMER_13_BIT
+#define LEDC_FREQUENCY (50)
+#define LEDC_DUTY_MIN 307
+#define LEDC_DUTY_MAX 944 
 
 bool dSense = false;
 bool dsbelt = false;
@@ -41,6 +50,31 @@ static hd44780_t lcd = {
             .bl = HD44780_NOT_USED
         }
     };
+
+static void example_ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 50 Hz
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .timer_sel      = LEDC_TIMER,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0
+    };
+ledc_channel_config(&ledc_channel);
+}
 
 
 /**
@@ -194,6 +228,7 @@ void lcdINIT(void *pvParameters)
 void app_main(void) {
     adcConfig();
     pinConfig();
+    example_ledc_init();
     bool initial_message = true;
     bool engineRunning = false;
     bool killEngine = false;
@@ -202,19 +237,34 @@ void app_main(void) {
         bool ignitEn = debounce(ignitionEn);
         int wiperSpeed = speedSelection(potentiometerRead());
         bool ready = IgnitionReady();
+        hd44780_clear(&lcd);
         if (wiperSpeed == 0){
             hd44780_clear(&lcd);
             hd44780_puts(&lcd, "WIPERS OFF");
         }
-        if (wiperSpeed == 1){
+        if (wiperSpeed == 1 && engineRunning){
             hd44780_clear(&lcd);
             hd44780_puts(&lcd, "WIPERS INTERMITTENT");
+            for (int i = 0; i <= 1800; i++){
+                if (i > 600 && i < 1200){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MIN);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                }
+                if ( i >= 1200 && i < 1800){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MAX);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                }
+                if (i >= 1800){
+                    i = 0;
+                }
+                vTaskDelay(25 / portTICK_PERIOD_MS);
+            }
         }
-        if (wiperSpeed == 2){
+        if (wiperSpeed == 2 && engineRunning){
             hd44780_clear(&lcd);
             hd44780_puts(&lcd, "WIPERS LOW");
         }
-        if (wiperSpeed == 3){
+        if (wiperSpeed == 3 && engineRunning){
             hd44780_clear(&lcd);
             hd44780_puts(&lcd, "WIPERS HIGH");
         }
@@ -270,6 +320,5 @@ void app_main(void) {
             gpio_set_level (Alarm, 0);
         }
         vTaskDelay(25 / portTICK_PERIOD_MS);
-
     }
 }
