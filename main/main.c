c14a7fa7284dc8112c3d3a1cc877b4ba1115b166
sebCil <sdcilia24@gmail.com>
2026-02-13 17:48:21 -0500

    final changes

--- main/main.c
+++ main/main.c
@@ -10,7 +10,6 @@
 #include <inttypes.h>
 #include "driver/ledc.h"
 
-
 #define ignitionLED 11
 #define engineLED 3
 #define ignitionEn 2
@@ -22,16 +21,15 @@
 #define adcAtten ADC_ATTEN_DB_12
 #define bitWidth ADC_BITWIDTH_12
 #define wiperPoten ADC_CHANNEL_3
-#define intermittentChan ADC_CHANNEL_3
+#define intermittentChan ADC_CHANNEL_5
 #define LEDC_TIMER LEDC_TIMER_0
 #define LEDC_MODE LEDC_LOW_SPEED_MODE
 #define LEDC_OUTPUT_IO 7
 #define LEDC_CHANNEL LEDC_CHANNEL_0
 #define LEDC_DUTY_RES LEDC_TIMER_13_BIT
 #define LEDC_FREQUENCY 50
-#define LEDC_DUTY_MIN 307
-#define LEDC_DUTY_MAX 944 
-
+#define LEDC_DUTY_NINETY 580
+#define LEDC_DUTY_ZERO 944 
 
 bool dSense = false;
 bool dsbelt = false;
@@ -269,51 +267,166 @@ void lcdINIT(void *pvParameters)
 }
 
 void app_main(void) {
+
     adcConfig();
     pinConfig();
     example_ledc_init();
+    lcdINIT(NULL);
+    int lastSelectionSpeed = -1;
+    int lastSelecInter = -1;
     bool initialMessage = true;
     bool engineRunning = false;
-    bool killEngine = false;
+    bool alarmOff = true;
     int counter = 0;
-    lcdINIT(NULL);
+    int Threshold = 14;
+
     while(1){
         bool ignitEn = debounce(ignitionEn);
         int wiperSpeed = speedSelection(wiperPotentiometerRead());
-       // int interSelect = intermDelaySelection(intermittentPotenRead());
+        int interSelect = intermDelaySelection(intermittentPotenRead());
         bool ready = IgnitionReady();
-        hd44780_clear(&lcd);
-        if (wiperSpeed == 0 && engineRunning){
+        if (!engineRunning){
+            hd44780_clear(&lcd);
+        }
+        if ((lastSelectionSpeed != wiperSpeed) && engineRunning){
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_ZERO);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+            counter = 0;
+            hd44780_gotoxy(&lcd, 0, 0);
+            if (wiperSpeed == 0){
             hd44780_clear(&lcd);
             hd44780_puts(&lcd, "WIPERS OFF");
+            }
+            if (wiperSpeed == 1){
+                hd44780_clear(&lcd);
+                hd44780_puts(&lcd, "WIPERS INTERMITTENT");
+            }
+            if (wiperSpeed == 2){
+                hd44780_clear(&lcd);
+                hd44780_puts(&lcd, "WIPERS LOW");
+            }
+            if (wiperSpeed == 3){
+                hd44780_clear(&lcd);
+                hd44780_puts(&lcd, "WIPERS HIGH");
+            }
+            lastSelectionSpeed = wiperSpeed;
         }
 
-        if (wiperSpeed == 1 && engineRunning){
-            hd44780_clear(&lcd);
-           // printf("%")
-            hd44780_puts(&lcd, "WIPERS INTERMITTENT");
-                if (counter < 13){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MIN);
+        if ((lastSelecInter != interSelect) && wiperSpeed == 1){
+            ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_ZERO);
+            ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+            hd44780_gotoxy(&lcd, 0, 1);
+            hd44780_puts(&lcd, "                     ");
+            if (interSelect == 0){
+                hd44780_gotoxy(&lcd, 0, 1);
+                hd44780_puts(&lcd, "SHORT");
+            }
+            else if (interSelect == 1){
+                hd44780_gotoxy(&lcd, 0, 1);
+                hd44780_puts(&lcd, "MED");
+            }
+            else{
+                hd44780_gotoxy(&lcd, 0, 1);
+                hd44780_puts(&lcd, "LONG");                    
+            }
+            lastSelecInter = interSelect;
+        }
+
+        if (engineRunning){
+            if (wiperSpeed == 0){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_ZERO);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+        }
+        if (wiperSpeed == 1 || wiperSpeed == 2)
+        {
+            if (counter < Threshold){
+                    int LEDC_TURN_1 = LEDC_DUTY_NINETY+ (LEDC_DUTY_ZERO-LEDC_DUTY_NINETY)*(((float)counter+1)/Threshold);
+                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_TURN_1);
                     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
                 }
-                else if (counter >= 13 && counter < 26){
-                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MAX);
+            else if (counter >= Threshold && counter < Threshold * 2){
+                    int LEDC_TURN_2;
+                    if(counter<Threshold*2)
+                    {
+                        LEDC_TURN_2 = LEDC_DUTY_ZERO-LEDC_DUTY_ZERO*(((float)counter-Threshold+1)/(Threshold))+
+                        LEDC_DUTY_NINETY*(((float)counter-Threshold+1)/(Threshold));
+                    }
+                    else
+                    {
+                        LEDC_TURN_2 = LEDC_DUTY_NINETY;
+                    }
+                    ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_TURN_2);
                     ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
                 }
-                else if (counter >= 54){
+            if (wiperSpeed == 1 && interSelect == 0){
+                    if (counter >= (int)(Threshold * 2.66)){
+                        counter = 0;
+                     }
+                }
+            if (wiperSpeed == 1 && interSelect == 1){
+                     if (counter >= Threshold * 4){
                     counter = 0;
+                     }
                 }
+            if (wiperSpeed == 1 && interSelect == 2){
+                    if (counter >= (int)(Threshold * 5.33)){
+                        counter = 0;
+                    }
+                }
+            if(wiperSpeed == 2 && counter >= Threshold*2)
+            {
+                counter=0;
+            }
             counter++;
         }
-        
+
+        if (wiperSpeed == 3){
+            for (int i = LEDC_DUTY_ZERO; i > LEDC_DUTY_NINETY; i -= 20){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, i);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                vTaskDelay(25 / portTICK_PERIOD_MS);
+            }
+            for (int j = LEDC_DUTY_NINETY; j < LEDC_DUTY_ZERO; j += 20){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, j);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                vTaskDelay(25 / portTICK_PERIOD_MS);
+            }
+
+        }
+
+        }
+
+        /*
         if (wiperSpeed == 2 && engineRunning){
-            hd44780_clear(&lcd);
-            hd44780_puts(&lcd, "WIPERS LOW");
+            if (counter < Threshold){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MIN);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                }
+            else if (counter >= Threshold && counter < Threshold * 2){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MAX);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                }
+            if (counter >= Threshold * 5){
+                    counter = 0;
+            }
+            counter++;
         }
+
         if (wiperSpeed == 3 && engineRunning){
-            hd44780_clear(&lcd);
-            hd44780_puts(&lcd, "WIPERS HIGH");
+            if (counter < Threshold){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MIN);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                }
+            else if (counter >= Threshold && counter < Threshold * 2){
+                ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY_MAX);
+                ledc_update_duty(LEDC_MODE, LEDC_CHANNEL);
+                }
+            if (counter >= Threshold * 2){
+                    counter = 0;
+            }
+            counter++;
         }
+        */
 
         if (dSense && initialMessage){
             printf("Welcome to enhanced Alarm system model 218 -W25\n");
@@ -323,24 +436,35 @@ void app_main(void) {
         if (ready && !engineRunning){
             gpio_set_level(ignitionLED, 1);
         }
-        else{gpio_set_level(ignitionLED, 0);}
+        else{
+            gpio_set_level(ignitionLED, 0);
+        }
     
-
         if(ignitEn){
+            vTaskDelay (25 / portTICK_PERIOD_MS);
             if (engineRunning){
             printf("engine stopping...\n");
             gpio_set_level(engineLED, 0);
             engineRunning = false;
-            killEngine = true;
+            alarmOff = true;
+            lastSelectionSpeed = -1;
+            lastSelecInter = -1;
+            continue;
             }
             if (ready) {
+                lastSelectionSpeed = -1;
+                lastSelecInter = -1;
                 engineRunning = true;
-                killEngine = false;
+                alarmOff = true;
                 printf("engine starting...\n");
                 gpio_set_level(ignitionLED, 0);
                 gpio_set_level(engineLED, 1);
+                continue;
             }
-            if (!engineRunning && !killEngine){
+            else{
+                alarmOff = false;
+            }
+            if (!alarmOff){
                 gpio_set_level(Alarm, 1);
                 if (!dSense){
                     printf("Driver seat not occupied\n");
@@ -356,14 +480,14 @@ void app_main(void) {
                 if (!psbelt){
                     printf("Passenger seatbelt not fastened\n");
                 }
-                engineRunning = false;
-                killEngine = false;
                 vTaskDelay (2000/ portTICK_PERIOD_MS);
+                alarmOff = true;
             }
         }
         else {
             gpio_set_level (Alarm, 0);
         }
         vTaskDelay(25 / portTICK_PERIOD_MS);
+
     }
 }
